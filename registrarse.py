82b29911e9adf7bde34020e0d0f81e4f067ed732
AriEstudioso <aridaneestudioso@gmail.com>
2026-03-23 18:12:46 +0100

    Creación parcial de una función para comprobar que la contraseña es correcta.

--- registrarse.py
+++ registrarse.py
@@ -1,22 +1,25 @@
 import hashlib
 
-
 # Función para encriptar la contraseña
 def encriptar_contraseña(contraseña):
     return hashlib.sha256(contraseña.encode()).hexdigest()
 
+
+def comprobar_contraseña(contraseña, confirmar_contraseña):
+    if contraseña != confirmar_contraseña:
+        print("Error: Las contraseñas no coinciden.")
+        return
 
 # Función para registrar usuario
 def registrar_usuario():
     print("=== Registro de Usuario ===")
     nombre = input("Nombre: ").strip()
     email = input("Email: ").strip()
+    telefono = input("Número de Teléfono: ").strip()
     contraseña = input("Contraseña: ").strip()
     confirmar_contraseña = input("Confirmar contraseña: ").strip()
 
-    if contraseña != confirmar_contraseña:
-        print("Error: Las contraseñas no coinciden.")
-        return
+    comprobar_contraseña(contraseña, confirmar_contraseña)
 
     contraseña_encriptada = encriptar_contraseña(contraseña)
 
