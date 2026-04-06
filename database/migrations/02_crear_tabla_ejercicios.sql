fc9738d69f84555b629548df94312e9d60d19447
oscar <oscar@ulpgc.es>
2026-04-06 17:37:30 +0100

    Borrar archivos que sobran

--- database/migrations/02_crear_tabla_ejercicios.sql
+++
@@ -1,8 +0,0 @@
--- Migración 02: Crear tabla de ejercicios (Catálogo Base)
-
-CREATE TABLE ejercicios (
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    grupo_muscular VARCHAR(50) NOT NULL,
-    descripcion TEXT
-);
