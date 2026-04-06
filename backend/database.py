fea6e147e869e7a27c7331ddb32a42ccce204ef3
AriEstudioso <aridaneestudioso@gmail.com>
2026-04-06 18:35:35 +0200

    Eliminacion de archivos.py

--- backend/database.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def conectar():
-    return psycopg2.connect(
-        host="localhost",
-        database="rumbofit",
-        user="postgres",
-        password="tu_password"
-    )
